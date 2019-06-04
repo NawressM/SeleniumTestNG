fa970ed6377bcca0e302c5f0324bcc2bb9184604
NawressM <nawrass.mn@gmail.com>
2019-06-04 14:10:06 +0200

    Booking Flow until Availability Page

--- src/test/java/tuto/eclipse/jal/selenium/pageObjects/avaiPage/AvaiPageIDs.java
+++ src/test/java/tuto/eclipse/jal/selenium/pageObjects/avaiPage/AvaiPageIDs.java
@@ -2,5 +2,6 @@ package tuto.eclipse.jal.selenium.pageObjects.avaiPage;
 
 public class AvaiPageIDs {
 
-	public static final String 	LOGIN_RADIO_BUTTON_ID = "login-jmb";
+	public static final String LOGIN_RADIO_BUTTON_ID = "login-jmb";
+	public static final String CONTIUNE_BUTTON_ID = "continueButton";
 }
